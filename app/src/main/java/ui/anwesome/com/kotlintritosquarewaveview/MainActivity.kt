4b704e61dad72473318d53f0ef1d35be132f0c12
Anwesh43 <anweshthecool0@gmail.com>
2018-04-06 02:57:00 +0530

    adding view in activity

--- app/src/main/java/ui/anwesome/com/kotlintritosquarewaveview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintritosquarewaveview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlintritosquarewaveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.triangletosquareview.TriToSquareWaveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        TriToSquareWaveView.create(this)
     }
 }
