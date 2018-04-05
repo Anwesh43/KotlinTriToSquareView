c127d5b0ff843747d4a4ce5e3a7bc65a4c2b8a9f
Anwesh43 <anweshthecool0@gmail.com>
2018-04-06 01:26:46 +0530

    adding TriangleToSquareWaveView

--- triangletosquareview/src/main/java/ui/anwesome/com/triangletosquareview/TriToSquareWaveView.kt
+++ triangletosquareview/src/main/java/ui/anwesome/com/triangletosquareview/TriToSquareWaveView.kt
@@ -3,3 +3,26 @@ package ui.anwesome.com.triangletosquareview
 /**
  * Created by anweshmishra on 06/04/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+
+class TriToSquareWaveView (ctx : Context) : View(ctx) {
+
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
